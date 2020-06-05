beee6b0b1ace063b22c30192b50b4c8cafa2fc86
Amanda Mourao <amoraesd@uoregon.edu>
2020-06-05 14:01:42 -0700

    Complete RESTful Web App, Part 3

--- server-8.js
+++ server-8.js
@@ -3,66 +3,69 @@
 
 // initialize
 
-const
-  port = 8888,
-  bodyParser = require('body-parser'),
-  express = require('express'),
+const port = 8888,
+  bodyParser = require("body-parser"),
+  express = require("express"),
   app = express(),
-  db = [{
-    "id": 1,
-    "name": "Argus Filch",
-    "number": "800-Hog-Wart"
-  }];
+  db = [
+    {
+      id: 1,
+      name: "Argus Filch",
+      number: "800-Hog-Wart",
+    },
+  ];
 
 //if the client directory contains an "index.html" web page
 //  it will be displayed as the default document
 app.use(express.static(__dirname + "/client"));
 
 // Parse data from incoming Post requests
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(bodyParser.json());
 
 // enable CORS
 app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', '*');
+  res.append("Access-Control-Allow-Origin", "*");
   next();
 });
 
 // endpoint to Get all tel numbers
-app.get('/telnums', (req, res) => {
+app.get("/telnums", (req, res) => {
   res.status(200).send({
-    success: 'true',
-    message: 'tel numbers retrieved successfully',
-    tel_nums: db
-  })
+    success: "true",
+    message: "tel numbers retrieved successfully",
+    tel_nums: db,
+  });
 });
 
 // endpoint to add a tel number
-app.post('/addnum', (req, res) => {
+app.post("/addnum", (req, res) => {
   const telnum = {
     id: db.length + 1,
     name: req.body.name,
-    number: req.body.number
-  }
+    number: req.body.number,
+  };
   db.push(telnum); //add record to local db
   return res.status(201).send({
-    success: 'true',
-    message: 'Telephone Number added successfully',
-    telnum
-  })
+    success: "true",
+    message: "Telephone Number added successfully",
+    telnum,
+  });
 });
 
 //endpoint to get a single tel number
 //Examples: /telnum/1, /telnum/7, /telnum/11, ...
-app.get('/telnum/:id', (req, res, next) => {
+app.get("/telnum/:id", (req, res, next) => {
   const id = parseInt(req.params.id, 10);
   db.map((telnum) => {
     if (telnum.id === id) {
       return res.status(200).send({
-        success: 'true',
-        message: 'telephone number retrieved successfully',
+        success: "true",
+        message: "telephone number retrieved successfully",
         telnum,
       });
     }
@@ -73,27 +76,27 @@ app.get('/telnum/:id', (req, res, next) => {
   }
   //if we did not find the target
   return res.status(404).send({
-    success: 'false',
-    message: 'telphone number does not exist',
+    success: "false",
+    message: "telphone number does not exist",
   });
 });
 
 //Endpoint to Delete a single tel number
-app.delete('/deletenum/:id', (req, res) => {
+app.delete("/deletenum/:id", (req, res) => {
   const id = parseInt(req.params.id, 10);
 
   db.map((telnum, index) => {
     if (telnum.id === id) {
       db.splice(index, 1);
       return res.status(200).send({
-        success: 'true',
-        message: 'Telephone number deleted successfuly',
+        success: "true",
+        message: "Telephone number deleted successfuly",
       });
     }
   });
   return res.status(404).send({
-    success: 'false',
-    message: 'Telephone number not found',
+    success: "false",
+    message: "Telephone number not found",
   });
 });
 
